2308b0cc2c5e7d57e110a3e7bb9af56d77fbc7bc
hillbillyjoe41 <157071173+hillbillyjoe41@users.noreply.github.com>
2025-05-08 17:34:45 -0500

    AuthContext added

--- JAZA-FRONT/src/pages/context/AuthContext.jsx
+++ JAZA-FRONT/src/pages/context/AuthContext.jsx
@@ -6,6 +6,7 @@ import { api } from "../services/auth-api";
 
 const AuthContext = createContext();
 
+// cleans up claims to nice and neat strings
 const normalizeClaims = (decoded) => ({
   email:
     decoded[
@@ -18,6 +19,7 @@ const normalizeClaims = (decoded) => ({
   name: decoded["http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name"],
 });
 
+// finite state machine for authContext
 const authReducer = (state, action) => {
     switch(action.type){
         case "LOGIN":
@@ -29,15 +31,67 @@ const authReducer = (state, action) => {
     }
 };
 
+// main component
 export const AuthProvider = ({ children }) => {
+    // initialize default state values
     const [state, dispatch] = useReducer(authReducer, {
         user: null,
         isAuthenticated: false
     });
 
-
+    // function to check if token exists and is not expired
     const checkTokenValidity = () => {
+        // accesses localstorage to get token
+        const token = localStorage.getItem("jwt");
+        if(!token){
+            return false;
+        }
+        try{
+            const decoded = jwtDecode(token);
+            return decoded.exp * 1000 > Date.now();
+        } catch(error){
+            return false;
+        }
+    };
 
+    // function to handle login action
+    const login = async (credentials) => {
+        // sends post request to api service with base url attached to front and
+        // credentials attached to body
+        const response = await api.post("auth/login", credentials);
+        const token = response.data.token;
+        // assigns token to localStorage
+        localStorage.setItem("jwt", token);
+        const decoded = jwtDecode(token);
+        dispatch({type: "LOGIN", payload: normalizeClaims(decoded)});
+        return true;
+    };
 
-    }
-}
+    // function to handle logout action
+    const logout = () => {
+        localStorage.removeItem("jwt");
+        dispatch({type: "LOGOUT"});
+    };
+
+    // user should remain logged in when page refreshes
+    // useEffect triggers once when component is mounting
+    // simply checks if the user has a valid token and
+    // logs the user in if they do
+    useEffect(() => {
+        if(checkTokenValidity()){
+            const token = localStorage.getItem("jwt");
+            const decode = jwtDecode(token);
+            dispatch({type: "LOGIN", payload: normalizeClaims(decode)});
+        }
+    }, []);
+
+    return(
+        <AuthContext.Provider
+            value = {{...state, login, logout, checkTokenValidity}}
+        >
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export const useAuth = () => useContext(AuthContext);
