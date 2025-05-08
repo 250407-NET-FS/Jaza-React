d8cafd91eb9c584726459191c18654beee6700c1
Amathost <124377028+Amathost@users.noreply.github.com>
2025-05-08 15:10:58 -0400

    fixed property

--- Project-2.Models/DTOs/PropertyDTO/PropertyUpdateDTO.cs
+++ Project-2.Models/DTOs/PropertyDTO/PropertyUpdateDTO.cs
@@ -14,9 +14,9 @@ public class PropertyUpdateDTO
     public decimal? StartingPrice { get; set; }
     public int? Bedrooms { get; set; }
     public decimal? Bathrooms { get; set; }
-    public int Garages { get; set; }
-    public int Pools { get; set; }
-    public bool HasBasement { get; set; }
+    public int? Garages { get; set; }
+    public int? Pools { get; set; }
+    public bool? HasBasement { get; set; }
     public DateTime? ListDate { get; set; }
     public Guid? OwnerID { get; set; }
     public bool? ForSale { get; set; }
