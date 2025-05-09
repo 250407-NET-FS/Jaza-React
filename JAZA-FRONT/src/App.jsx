92324aeb0ee0cbb7c39959a755435c4c31015270
hillbillyjoe41 <157071173+hillbillyjoe41@users.noreply.github.com>
2025-05-09 10:04:11 -0500

    fixed merge

--- JAZA-FRONT/src/App.jsx
+++ JAZA-FRONT/src/App.jsx
@@ -4,20 +4,23 @@ import Home from "./pages/Home"; // or './Home' if not in a folder
 import Dashboard from "./pages/admin/Dashboard";
 import PropertyList from "./pages/admin/PropertyList";
 import UserList from "./pages/admin/UserList";
+import { AuthProvider } from "./pages/context/AuthContext";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        
-        <Route path="/" element={<Home />} />
-        {/* <Route path='/listings' element={<Listings />} /> */}
-        <Route path="/admin/Dashboard" element={<Dashboard />} />
-        <Route path="/UserList" element={<UserList />} />
-        <Route path="/PropertyList" element={<PropertyList />} />
-        {/* Add more routes as needed */}
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          
+          <Route path="/" element={<Home />} />
+          {/* <Route path='/listings' element={<Listings />} /> */}
+          <Route path="/admin/Dashboard" element={<Dashboard />} />
+          <Route path="/UserList" element={<UserList />} />
+          <Route path="/PropertyList" element={<PropertyList />} />
+          {/* Add more routes as needed */}
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
 
