0aa7afc9546720c1653a7df435797b3d3b56e45f
Andric Tam <andtam1999@gmail.com>
2025-05-13 15:37:31 -0400

    Search bar fixes

--- JAZA-FRONT/src/pages/properties/SearchBar.jsx
+++ JAZA-FRONT/src/pages/properties/SearchBar.jsx
@@ -4,15 +4,17 @@ import { Autocomplete, TextField } from "@mui/material";
 
 const SearchBar = () => {
     const [loading, setLoading] = useState(false);
-    const [options, setOptions] = useState([]);
+    const [optionList, setOptionList] = useState([]);
     const [open, setOpen] = useState(false);
+    const [error, setError] = useState(null);
+    const [selectedOption, setSelectedOption] = useState(null);
 
     const fetchData = async (value) => {
         setLoading(true);
         try {
             await api.get(`properties/search/${value}`)
             .then(res => res.data)
-            .then(data => setOptions(data));
+            .then(data => setOptionList(data));
         }
         catch (error) {
             console.log(error.status)
@@ -37,12 +39,11 @@ const SearchBar = () => {
     }
 
     const handleKeyDown = async (event) => {
-        console.log(value);
-        if (event.key !== 'Enter' || value.length < 3) {
+        if (event.key !== 'Enter' || event.target.value.trim().length < 1) {
             return;
         }
-        console.log("here!");
         try {
+            console.log(selectedOption.propertyID);
             // unfinished final query that should redirect to property page with results
         } 
         catch (error) {
@@ -57,11 +58,12 @@ const SearchBar = () => {
         <Autocomplete 
             open={open}
             loading={loading}
-            options={options}
+            options={optionList}
             forcePopupIcon={false}
             filterOptions={(option) => option} // Override default filterOptions behavior as recommended by MUI
             getOptionLabel={(option) => option.streetAddress}
             onInputChange={handleChange}
+            onChange={(event, value) => setSelectedOption(value)}
             onClose={() => setOpen(false)}
             renderInput={(params) => (
                 <TextField
