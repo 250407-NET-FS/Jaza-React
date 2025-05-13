2067dd40a6003adb673df52c365733dd1a3fb654
beemanz134 <beeman134@gmail.com>
2025-05-13 10:27:55 -0500

    added photos to proporty cards and make clickable

--- JAZA-FRONT/src/pages/properties/PropertyDetails.jsx
+++ JAZA-FRONT/src/pages/properties/PropertyDetails.jsx
@@ -2,7 +2,7 @@ import { Container, Grid, Icon, IconButton } from '@mui/material'
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import React, { useState } from 'react'
-import houseImage from '../../assets/house.png';
+import apartmentImage from '../../assets/apartment.png';
 
 function PropertyDetails({ property }) {
     let daysListed = Math.abs(Date() - property.listDate.getTime) / (1000 * 60 * 60 * 24);
@@ -14,11 +14,20 @@ function PropertyDetails({ property }) {
         <Container>
             <Grid container>
                 <Grid size={12}>
-                    <img src={property.imageLink || houseImage} alt={property.streetAddress}></img>
-                    <IconButton aria-label='save'>
-                        <FavoriteIcon>
-                            onClick={() => setIsFavorite(!isFavorite)}
-                        </FavoriteIcon>
+                    <img
+                        // src={property.imageLink || houseImage}
+                        // alt={property.streetAddress}
+                        src={apartmentImage}
+                        alt="Property address"
+                        style={{
+                            width: '45%',
+                            height: 'auto',
+                            objectFit: 'cover',
+                            borderRadius: '8px',
+                        }}
+                    />
+                    <IconButton aria-label='save' onClick={() => setIsFavorite(!isFavorite)}>
+                        <FavoriteIcon />
                     </IconButton>
                 </Grid>
                 <Grid size={4}>
--- JAZA-FRONT/src/pages/properties/UserPropertyList.jsx
+++ JAZA-FRONT/src/pages/properties/UserPropertyList.jsx
@@ -3,6 +3,7 @@ import { useProperty } from "../context/PropertyContext";
 import React, { useEffect, useState } from 'react'
 import Popup from "reactjs-popup";
 import PropertyDetails from './PropertyDetails';
+import houseImage from '../../assets/house.png';
 
 function UserPropertyList() {
     const {
@@ -55,6 +56,17 @@ function UserPropertyList() {
                                 </Popup>
                                 <CardContent>
                                     <button onClick={() => handleClick(p.propertyID)} style={{ all: 'unset', cursor: 'pointer' }}>
+                                        <img
+                                            src={houseImage}
+                                            // alt={property.streetAddress}
+                                            alt="Property address"
+                                            style={{
+                                                width: '45%',
+                                                height: 'auto',
+                                                objectFit: 'cover',
+                                                borderRadius: '8px',
+                                            }}
+                                        />
                                         <h3>{p.startingPrice}</h3>
                                         <p>{p.bedrooms} | {p.bathrooms} - {p.forSale}</p>
                                         <p>{p.streetAddress}, {p.city}, {p.state}, {p.country} {p.zipCode}</p>
