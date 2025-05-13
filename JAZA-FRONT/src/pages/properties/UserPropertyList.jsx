db53efc05eb1c84868f84289cc9375f730c73a37
hillbillyjoe41 <157071173+hillbillyjoe41@users.noreply.github.com>
2025-05-12 22:43:01 -0500

    fixes

--- JAZA-FRONT/src/pages/properties/UserPropertyList.jsx
+++ JAZA-FRONT/src/pages/properties/UserPropertyList.jsx
@@ -1,6 +1,7 @@
 import { Card, CardContent, Container, Grid } from '@mui/material'
 import { useProperty } from "../context/PropertyContext";
 import React, {useEffect} from 'react'
+import Retrieve from './Retrieve';
 
 function UserPropertyList() {
     const { 
@@ -12,18 +13,24 @@ function UserPropertyList() {
         fetchPropertyList()
     }, [fetchPropertyList]);
 
+    const handleClick = (propertyID) => {
+
+    };
+
   return (
     <Container>
         <h1>Homes</h1>
         <Grid container>
             {
                 propertyList.map(p => 
-                    <Grid size={2}>
-                        <Card key={p.propertyID}>
+                    <Grid size={2} key={p.propertyID}>
+                        <Card>
                             <CardContent>
+                                <button onClick={() => handleClick(p.propertyID)}style={{ all: 'unset', cursor: 'pointer' }}>
                                 <h3>{p.startingPrice}</h3>
                                 <p>{p.bedrooms} | {p.bathrooms} - {p.forSale}</p>
                                 <p>{p.streetAddress}, {p.city}, {p.state}, {p.country} {p.zipCode}</p>
+                                </button>
                             </CardContent>
                         </Card>
                     </Grid>
